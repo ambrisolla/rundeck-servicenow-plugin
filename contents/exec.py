c5fed00e167d0681af67806405ce0851d0e7911e
André Brisolla <andremuzel@gmail.com>
2023-06-19 12:44:38 -0300

    Do not verify SSL

--- contents/exec.py
+++ contents/exec.py
@@ -173,7 +173,8 @@ class RundeckServiceNowApproval:
             f'{self.ARGUMENTS.RD_SERVER}/{endpoint}',
             headers={            
                 'X-Rundeck-Auth-Token': self.ARGUMENTS.RD_TOKEN,
-                'Accept': 'application/json' })
+                'Accept': 'application/json',
+                verify=False})
         if req.status_code == 200:
             return req.json()
         else:
